57cacab932d42c0ab12da218943c5811d6930eda
David Lin <d2955388365@gmail.com>
2020-07-05 23:03:27 +0800

    add range

--- guess.py
+++ guess.py
@@ -1,12 +1,20 @@
-import random 
-r = random.randint(1,100)
+import random
+start = input('請決定隨機數字範圍開始質')
+end = input('請決定隨機數字範圍結束質') 
+start = int(start)
+end = int(end)
+r = random.randint(start,end)
+count = 0
 while True : 
+  count += 1
   num = input('請猜數字')
   num = int(num)
   if num == r :
      print('你猜對了')
      break 
+     print('這是你猜的第',count,'次')
   elif num > r :
      print('比答案大')
   elif num < r :
   	 print('比答案小')
+  print('這是你猜的第',count,'次')
